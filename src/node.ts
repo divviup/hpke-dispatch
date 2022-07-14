281cc37b6535d64b22d1da5bd6513f6c5c0fa279
Jacob Rothstein <hi@jbr.me>
2022-07-14 15:23:32 -0700

    fix node

--- src/node.ts
+++ src/node.ts
@@ -3,4 +3,4 @@ import init from "../pkg/node/hpke_dispatch.js";
 //this is exported so it doesn't get dead-code-eliminated
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 export const __wasm = init(wasm);
-export * from "../pkg/web/hpke_dispatch.js";
+export * from "../pkg/node/hpke_dispatch.js";
