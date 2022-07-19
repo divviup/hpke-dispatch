b25a527e715a44708480e5dfe2e482f14087168c
Jacob Rothstein <hi@jbr.me>
2022-07-19 14:02:22 -0700

    use a different build strategy
    keep index.ts in git

--- pkg/node/index.ts
+++ pkg/node/index.ts
@@ -1,6 +1,6 @@
-import wasm from "../pkg/node/hpke_dispatch_bg.wasm";
-import init from "../pkg/node/hpke_dispatch.js";
+import wasm from "./hpke_dispatch_bg.wasm";
+import init from "./hpke_dispatch.js";
 //this is exported so it doesn't get dead-code-eliminated
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 export const __wasm = init(wasm);
-export * from "../pkg/node/hpke_dispatch.js";
+export * from "./hpke_dispatch.js";
