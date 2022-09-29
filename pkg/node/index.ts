349500213c5193a3f38e6411dc36f2262d22d544
Jacob Rothstein <hi@jbr.me>
2022-09-29 14:12:46 -0700

    use wasm-bindgen's fs.readFileSync loader in node (#17)
    * use wasm-bindgen's fs.readFileSync loader in node
    
    * remove index.ts
    
    * update rust deps
    
    * bump to 0.4.0 and add eslint-plugin because tooling was looking for it
    
    * bump cargo version

--- pkg/node/index.ts
+++
@@ -1,6 +0,0 @@
-import wasm from "./hpke_dispatch_bg.wasm";
-import init from "./hpke_dispatch.js";
-//this is exported so it doesn't get dead-code-eliminated
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-export const __wasm = init(wasm);
-export * from "./hpke_dispatch.js";
