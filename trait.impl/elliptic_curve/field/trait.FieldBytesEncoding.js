11fa16afd9cf5c8438fb9ef094ac80944c6a6ae8
divviup-github-automation <divviup-github-automation@users.noreply.github.com>
2025-03-24 20:39:07 +0000

    deploy: f9ced6e60c80a1ab36b2040cd506b39c8e73a68e

---
+++ trait.impl/elliptic_curve/field/trait.FieldBytesEncoding.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["p256",[["impl <a class=\"trait\" href=\"elliptic_curve/field/trait.FieldBytesEncoding.html\" title=\"trait elliptic_curve::field::FieldBytesEncoding\">FieldBytesEncoding</a>&lt;<a class=\"struct\" href=\"p256/struct.NistP256.html\" title=\"struct p256::NistP256\">NistP256</a>&gt; for <a class=\"type\" href=\"p256/type.U256.html\" title=\"type p256::U256\">U256</a>"]]],["p384",[["impl <a class=\"trait\" href=\"elliptic_curve/field/trait.FieldBytesEncoding.html\" title=\"trait elliptic_curve::field::FieldBytesEncoding\">FieldBytesEncoding</a>&lt;<a class=\"struct\" href=\"p384/struct.NistP384.html\" title=\"struct p384::NistP384\">NistP384</a>&gt; for <a class=\"type\" href=\"p384/type.U384.html\" title=\"type p384::U384\">U384</a>"]]],["p521",[["impl <a class=\"trait\" href=\"elliptic_curve/field/trait.FieldBytesEncoding.html\" title=\"trait elliptic_curve::field::FieldBytesEncoding\">FieldBytesEncoding</a>&lt;<a class=\"struct\" href=\"p521/struct.NistP521.html\" title=\"struct p521::NistP521\">NistP521</a>&gt; for <a class=\"type\" href=\"p521/type.U576.html\" title=\"type p521::U576\">U576</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[372,373,373]}
